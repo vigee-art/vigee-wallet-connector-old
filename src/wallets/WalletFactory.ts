61bb55f645297febddefe1639c61183aadf90ace
JuWeber99 <julianweber99@web.de>
2022-06-29 16:16:22 +0200

    enum change

--- src/wallets/WalletFactory.ts
+++ src/wallets/WalletFactory.ts
@@ -1,23 +1,27 @@
-import { ImplementedWallets, IWallet, Networks, WalletConstructor } from "../_types";
+import {
+  ImplementedWallets,
+  IWallet,
+  Networks,
+  WalletConstructor,
+} from "../_types";
 import AlgoSignerWallet from "./AlgosignerWallet";
 import MyAlgoWallet from "./MyAlgoWallet";
 import PeraWallet from "./PeraWallet";
 
 type WalletMap = Record<ImplementedWallets, WalletConstructor<IWallet>>;
 export abstract class Factory<T> {
-    abstract create(param: any, param2: any): T;
+  abstract create(param: any, param2: any): T;
 }
 export class WalletFactory {
-    constructor() {
-    }
+  constructor() {}
 
-    static create(network: Networks, wallet: ImplementedWallets): IWallet {
-        const walletMap: WalletMap = {
-            "MyAlgoWallet": MyAlgoWallet,
-            "AlgoSignerWallet": AlgoSignerWallet,
-            "PeraWallet": PeraWallet
-        };
-        console.log("Creating wallet..");
-        return new walletMap[wallet](network);
-    }
+  static create(network: Networks, wallet: ImplementedWallets): IWallet {
+    const walletMap: WalletMap = {
+      MYALGO: MyAlgoWallet,
+      ALGOSIGNER: AlgoSignerWallet,
+      PERA: PeraWallet,
+    };
+    console.log("Creating wallet..");
+    return new walletMap[wallet](network);
+  }
 }
